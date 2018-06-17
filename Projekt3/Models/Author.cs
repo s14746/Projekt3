69be3254721bf81a9492179791d812d96acb332c
Marlena Franckiewicz <s14746@pjwstk.edu.pl>
2018-06-17 20:47:39 +0200

    add models Author and Book

---
+++ Projekt3/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt3.Models
+{
+    public class Author
+    {
+     
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
